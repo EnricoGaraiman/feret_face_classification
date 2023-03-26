e505b77f350d068ee0de7431ab2c55fbeb6449a9
Enrico Garaiman <enrico7899@yahoo.com>
2023-03-26 21:37:17 +0300

    Feature: Dataset distribution + gender & race & YOB

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ DATASET = {
     'name': 'COLOR FERET FACE RECOGNITION',
     'images_dir': 'data/colorferet/converted_images/images',
     'ground_truths_dir': 'data/colorferet/converted_images/ground_truths',
+    'subjects_info': 'data/colorferet/converted_images/ground_truths/xml/subjects.xml',
     'test_files': {
         'data/colorferet/converted_images/partitions/dup1.txt',
         'data/colorferet/converted_images/partitions/dup2.txt',
@@ -27,11 +28,12 @@ DATASET = {
 """
 if __name__ == '__main__':
     # dataset visualisation todo
+    dataset_file.plot_dataset_visualisation(DATASET)
 
     # dataset
-    train_images_name, test_images_name = dataset_file.get_dataset_images_name(DATASET)
-    dataset_train, dataset_test = dataset_file.get_dataset(DATASET, train_images_name, test_images_name)
-
-    # dataset loaders
-    dataset_train_loader = dataset_file.get_dataset_loader(dataset_train, DATASET)
-    dataset_test_loader = dataset_file.get_dataset_loader(dataset_test, DATASET)
+    # train_images_name, test_images_name = dataset_file.get_dataset_images_name(DATASET)
+    # dataset_train, dataset_test = dataset_file.get_dataset(DATASET, train_images_name, test_images_name)
+    #
+    # # dataset loaders
+    # dataset_train_loader = dataset_file.get_dataset_loader(dataset_train, DATASET)
+    # dataset_test_loader = dataset_file.get_dataset_loader(dataset_test, DATASET)
--- src/dataset.py
+++ src/dataset.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import glob
 import numpy as np
 from torchvision import transforms
+import xmltodict
+import collections
 
 
 def get_dataset(DATASET, train_images_name, test_images_name):
@@ -46,6 +48,7 @@ def get_dataset_loader(dataset, DATASET):
 
     :param dataset: dataset
     :param DATASET: info
+    :return: dataset loader object
     """
 
     dataset_loader = DataLoader(
@@ -94,6 +97,14 @@ def check_dataset_loader(dataset_loader):
 
 
 def get_dataset_images_name(DATASET, split=True):
+    """
+    Get all images name for dataset train & test
+
+    :param DATASET: DATASET
+    :param split: split
+    :return: dataset images names
+    """
+
     paths = glob.glob(DATASET['images_dir'] + '/*/*.jpg')
 
     names = [path.split('\\')[-1].replace('.jpg', '') for path in paths]
@@ -112,3 +123,154 @@ def get_dataset_images_name(DATASET, split=True):
     train_images_name = set(names) - test_images_name
 
     return train_images_name, test_images_name
+
+
+def plot_dataset_visualisation(DATASET):
+    """
+    Plot all info for dataset
+
+    :param DATASET: DATASET
+    """
+    subjects_info = get_subjects_information(DATASET)
+
+    # dataset_distribution(DATASET)
+    dataset_others_distribution(DATASET, subjects_info)
+
+def get_subjects_information(DATASET):
+    """
+    Get subjects information from XML file converted to dictionary
+
+    :param DATASET: DATASET
+    :return: subjects information dict
+    """
+    with open(DATASET['subjects_info'], 'r', encoding='utf-8') as file:
+        xml_subjects = file.read()
+
+    return xmltodict.parse(xml_subjects)
+
+def dataset_distribution(DATASET):
+    """
+    Plot dataset classes distribution
+
+    :param DATASET: DATASET
+    """
+    paths_dir_subjects = glob.glob(DATASET['images_dir'] + '/*')
+    classes_idx = [int(idx.split('\\')[-1]) for idx in paths_dir_subjects]
+
+    dataset_classes = []
+    dataset_distr = []
+    x_ticks = []
+    x_labels = []
+    for i, dir in enumerate(paths_dir_subjects):
+        if int(dir.split('\\')[-1]) in classes_idx:
+            dataset_distr.append(len(glob.glob(dir + '/*.jpg')))
+            dataset_classes.append(dir.split('\\')[-1])
+        else:
+            dataset_distr.append(0)
+            dataset_classes.append(int(dataset_classes[-1]) + 1)
+
+        if i % 20 == 0:
+            x_ticks.append(i)
+            x_labels.append(dir.split('\\')[-1])
+
+    fig = plt.figure(figsize=(30, 10), dpi=900)
+    plt.bar(dataset_classes, dataset_distr, color='maroon')
+    plt.xlabel("Dataset class (subject ID)", fontsize=12)
+    plt.ylabel("No. of images", fontsize=12)
+    plt.title('Dataset distribution' + ' | ' + DATASET['name'], fontsize=18)
+    plt.margins(0)
+    ax = plt.gca()
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(x_labels, rotation=30, fontsize=8, horizontalalignment='right')
+    plt.tight_layout()
+    plt.savefig('data/results/dataset_distribution.jpg', dpi=fig.dpi)
+    # plt.show()
+
+    print('Min:', dataset_classes[np.argmin(dataset_distr)], dataset_distr[np.argmin(dataset_distr)])
+    print('Max:', dataset_classes[np.argmax(dataset_distr)], dataset_distr[np.argmax(dataset_distr)])
+
+
+def dataset_others_distribution(DATASET, subjects_info):
+    """
+    Plot dataset classes distribution by gender
+
+    :param DATASET: DATASET
+    :param subjects_info: subjects information
+    """
+
+    info = {
+        'gender': [],
+        'race': [],
+        'YOB': [],
+    }
+    for subject_info in subjects_info['Subjects']['Subject']:
+        info['gender'].append(subject_info['Gender']['@value'])
+        info['race'].append(subject_info['Race']['@value'])
+        info['YOB'].append(subject_info['YOB']['@value'])
+
+    # gender
+    counter = collections.Counter(info['gender'])
+    distr = []
+    data = []
+    for key, count in counter.items():
+        data.append(key)
+        distr.append(count)
+
+    fig = plt.figure(figsize=(10, 10), dpi=900)
+    plt.bar(data, distr, color='darkgreen', width=0.2)
+    plt.xlabel("Gender", fontsize=14)
+    plt.ylabel("No. of subjects", fontsize=14)
+    plt.title('Dataset gender distribution' + ' | ' + DATASET['name'], fontsize=20)
+    ax = plt.gca()
+    ax.tick_params(axis='both', labelsize=14)
+    plt.tight_layout()
+    plt.savefig('data/results/dataset_gender_distribution.jpg', dpi=fig.dpi)
+
+    # race
+    counter = collections.Counter(info['race'])
+    distr = []
+    data = []
+    for key, count in counter.items():
+        data.append(key)
+        distr.append(count)
+
+    fig = plt.figure(figsize=(10, 10), dpi=900)
+    plt.bar(data, distr, color='darkorange', width=0.5)
+    plt.xlabel("Race", fontsize=14)
+    plt.ylabel("No. of subjects", fontsize=14)
+    plt.title('Dataset race distribution' + ' | ' + DATASET['name'], fontsize=20)
+    ax = plt.gca()
+    ax.set_xticks(data)
+    ax.set_xticklabels(data, rotation=45, ha='right', rotation_mode='anchor')
+    ax.tick_params(axis='y', labelsize=14)
+    plt.tight_layout()
+    plt.savefig('data/results/dataset_race_distribution.jpg', dpi=fig.dpi)
+
+    # YOB
+    counter = collections.Counter(info['YOB'])
+    distr = []
+    data = []
+    for key, count in counter.items():
+        data.append(key)
+        distr.append(count)
+
+    fig = plt.figure(figsize=(10, 10), dpi=900)
+    plt.bar(data, distr, color='dodgerblue', width=0.5)
+    plt.xlabel("Year of born", fontsize=14)
+    plt.ylabel("No. of subjects", fontsize=14)
+    plt.title('Dataset YOB distribution' + ' | ' + DATASET['name'], fontsize=20)
+    ax = plt.gca()
+    ax.set_xticks(data)
+    ax.set_xticklabels(data, rotation=45, ha='right', rotation_mode='anchor')
+    ax.tick_params(axis='y', labelsize=14)
+    plt.tight_layout()
+    plt.savefig('data/results/dataset_yob_distribution.jpg', dpi=fig.dpi)
+
+    # todo
+    # mustache
+
+    # glasses
+
+    # beard
+
+    # pose
